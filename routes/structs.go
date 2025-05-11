9608252c2134b851c9eb69c82e581e26b2e4869f
sudatra <mail2sudatra@gmail.com>
2025-05-11 18:30:48 +0530

    added request and response types

--- routes/structs.go
+++ routes/structs.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"github.com/google/uuid"
+)
+
+type GenerateVacationIdeaRequest struct {
+	FavouriteSeason 					string   			`json:"favourite_season"`
+	Hobbies         					[]string 			`json:"hobbies"`
+	Budget          					int      			`json:"budget"`
+}
+
+type GenerateVacationIdeaResponse struct {
+	Id 							uuid.UUID						`json:"id"`
+	Completed				bool								`json:"completed"`
+}
+
+type GetVacationIdeaResponse struct {
+	Id							uuid.UUID						`json:"id"`
+	Completed				bool								`json:"completed"`
+	Idea						string							`json:"idea"`
+}
